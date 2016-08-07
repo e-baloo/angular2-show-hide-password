160d092f1b0badbfc3a2c680ead3a7da2c7ad756
henkie14 <hagens88@gmail.com>
2016-08-07 10:56:51 +0200

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from 'angular2/core';
+import {Component} from '@angular/core';
 import {ShowHideContainer} from './show-hide-container'
 import {ShowHideInput} from './show-hide-input'
 
