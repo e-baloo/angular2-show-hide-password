4af113a2fa01c452ddbe0881e60904fa48e2fd06
henkie14 <chris.hagens@gmail.com>
2016-10-05 14:48:57 +0200

    Updated to Angular 2.0.1

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,22 @@
 import {Component} from '@angular/core';
 import {ShowHideContainer} from './show-hide-container'
-import {ShowHideInput} from './show-hide-input'
 
 @Component({
     selector: 'show-hide-app',
     template: `
     <h1>Show/Hide Password</h1>
     <label>Password:</label>
-    <show-hide-container>
-       <input type="password" show-hide-input [ngModel]="password" />
-    </show-hide-container>
-  `})
+    <show-hide-container (changeInputType)="inputTypeChanged($event)">
+       <input type="{{type}}" [ngModel]="password" />
+    </show-hide-container>`
+})
 export class AppComponent {
     password = "secret";
+    type: string = "password";
+
+    inputTypeChanged(type:string){
+         console.log('test:' + type);
+       
+        this.type = type;
+    }
 }
--- app/show-hide-container.ts
+++ app/show-hide-container.ts
@@ -1,29 +1,26 @@
-import {Component, ContentChild} from '@angular/core';
-import {ShowHideInput} from './show-hide-input'
+import {Component, Output, EventEmitter} from '@angular/core';
 
 @Component({ selector: 'show-hide-container',
             template: `<ng-content></ng-content>
-                       <a (click)="toggleShow($event)">show/hide</a>`,
-            styles: ['.show-hide {padding-right: 16px;}'],
-            viewProviders: [ShowHideInput]
+                       <a (click)="toggleInput()">show/hide</a>`
 })
 export class ShowHideContainer 
 {
-    show = false;
-    
-    @ContentChild(ShowHideInput) input: ShowHideInput;
+    @Output() changeInputType: EventEmitter<string> = new EventEmitter();
+    type: string = "password";
     
     constructor(){}
    
-    toggleShow()
+    toggleInput():void
     {
-        this.show = !this.show;
-        console.log(this.input);
-        if (this.show){
-            this.input.changeType("text");
+        if (this.type==="password")
+        {
+            this.type = "text";
         }
-        else {
-            this.input.changeType("password");
+        else
+        {
+            this.type = "password";
         }
+        this.changeInputType.emit(this.type);
     }
 }
